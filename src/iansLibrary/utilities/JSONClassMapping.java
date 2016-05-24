8ad257c85511d8556b36557d9b21065335717248
kirkp1ia <kirkp1ia@aux-vm-bb9a98.central.cmich.local>
2016-05-24 09:53:45 -0400

    added some stuff

---
+++ src/iansLibrary/utilities/JSONClassMapping.java
@@ -0,0 +1,158 @@
+package iansLibrary.utilities;
+
+import java.lang.reflect.Array;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import kirkModels.config.Settings;
+import kirkModels.fields.ManyToManyField;
+import kirkModels.orm.Model;
+import kirkModels.orm.queries.Query;
+
+public final class JSONClassMapping {
+		
+		public static Object jsonFieldToObject(String key, Object jsonVal) throws ClassNotFoundException, NoSuchMethodException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+			Object toReturn = null;
+			
+			String type = key.split("#")[1];
+			Class typeClass = Class.forName(type);
+			if (type.substring(0, 2).equals("[L")) {
+				toReturn = Array.newInstance(typeClass, ((JSONArray) jsonVal).size());
+				
+				for (int i = 0; i < ((JSONArray) jsonVal).size(); i ++) {
+					Object javaVal = jsonAnyToObject(i);
+					Array.set(toReturn, i, javaVal);
+				}
+			}
+			
+			return toReturn;
+		}
+		
+		public static Object jsonAnyToObject(Object jsonVal) throws ClassNotFoundException, NoSuchMethodException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+			Object toReturn = null;
+			jsonVal = verifyType(jsonVal);
+			
+			if (jsonVal == null) {
+				toReturn = null;
+			} else if (jsonVal.getClass().isPrimitive() || jsonVal instanceof String || isWrapper(jsonVal)) {
+				toReturn = jsonVal;
+			} else if (jsonVal instanceof JSONArray) {
+				toReturn = jsonArrayToArray((JSONArray) jsonVal);
+			} else if (jsonVal instanceof JSONObject) {
+				toReturn = jsonObjectToObject((JSONObject) jsonVal);
+			}
+			return toReturn;
+		}
+		
+		public static Object jsonObjectToObject(JSONObject jsonVal) throws ClassNotFoundException, NoSuchMethodException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+			Object toReturn = null;
+//			System.out.println(jsonVal);
+			
+			//figure out how to see if a class is a subclass of Query
+			if (Query.class.isAssignableFrom(Class.forName((String) jsonVal.get("type")))) {
+				jsonVal.put("1#java.lang.String#_dbName", Settings.database.dbName);
+			}
+			
+			String className = (String) jsonVal.get("type");
+			Object[] paramValues = new Object[jsonVal.keySet().size()-1];
+			Class[] paramTypes = new Class[jsonVal.keySet().size()-1];
+			int i = 0;
+			for (Object key : jsonVal.keySet()) {
+				if (!((String) key).equals("type")) {
+					//set value at key to attribute of toReturn.
+					i = Integer.valueOf(((String) key).split("#")[0]) - 1;
+					Class type = Class.forName(((String) key).split("#")[1]);
+//					System.out.println("\tkey: " + key + " value: " + jsonVal.get(key));
+					Object value = null;
+					
+					if (type.equals(Class.class)) {
+						value = Class.forName((String) jsonVal.get(key));
+					} else {
+						value = jsonAnyToObject(jsonVal.get(key));
+					}
+//					System.out.println("\t\tkey: " + key + " value: " + value);
+					
+//					System.out.println("javaVal = " + value);
+					paramTypes[i] = type;
+					paramValues[i] = value;
+//					if(i == 2) System.out.println(Arrays.toString((Object[]) value));
+				}
+			}
+			
+			Constructor c = Class.forName(className).getConstructor(paramTypes);
+			try {
+				toReturn = c.newInstance(paramValues);
+			} catch (IllegalArgumentException | InvocationTargetException e) {
+				for (Object object : paramValues) {
+				}
+				e.printStackTrace();
+			}
+//			System.out.println(toReturn.getClass());
+			
+			return toReturn;
+		}
+		
+		public static Object jsonArrayToArray(JSONArray jsonVal) throws ClassNotFoundException, NoSuchMethodException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+			Object toReturn = null;
+			
+			String dataType = jsonVal.get(0).toString();
+			Class typeClass = Class.forName(dataType);
+			toReturn = Array.newInstance(typeClass, jsonVal.size() - 1);
+			
+//			System.out.println(jsonVal.size());
+			for (int i = 1; i < jsonVal.size(); i ++) {
+//				System.out.println("i: " + jsonVal.get(i));
+				Object javaVal = jsonAnyToObject(jsonVal.get(i));
+				try {
+					Array.set(toReturn, i-1, javaVal);
+				} catch (IllegalArgumentException e) {
+					throw e;
+				}
+			}
+			
+			return toReturn;
+		}
+		
+		private static Set<Class<?>> getWrapperTypes()
+	    {
+	        Set<Class<?>> ret = new HashSet<Class<?>>();
+	        ret.add(Boolean.class);
+	        ret.add(Character.class);
+	        ret.add(Byte.class);
+	        ret.add(Short.class);
+	        ret.add(Integer.class);
+	        ret.add(Long.class);
+	        ret.add(Float.class);
+	        ret.add(Double.class);
+	        ret.add(Void.class);
+	        return ret;
+	    }
+		
+		public static boolean isWrapper(Object o){
+			if (getWrapperTypes().contains(o.getClass())) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+		
+		public static Object verifyType(Object o) {
+			Object retVal = null;
+			
+			if (o.getClass().equals(Long.class) && (Long) o < Integer.MAX_VALUE) {
+				retVal = (Integer) ((Long) o).intValue();
+			} else {
+				retVal = o;
+			}
+			
+			return retVal;
+		}
+
+}
--- src/iansLibrary/utilities/ModdedDate.java
+++ src/iansLibrary/utilities/ModdedDate.java
@@ -2,77 +2,188 @@ package iansLibrary.utilities;
 
 import java.time.Month;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
 
 public class ModdedDate {
+	
+	/*
+	 * got these conversions off of google... hope they're accurate.
+	 */
+	public static final long MILLIS_IN_YEAR = 31540000000L;
+	public static final long MILLIS_IN_DAY = 86400000L;
+	public static final long MILLIS_IN_HOUR = 3600000L;
+	public static final long MILLIS_IN_MINUTE = 60000L;
+	public static final long MILLIS_IN_SECOND = 1000L;
+	
+	public static final int SECONDS_IN_MINUTE = 60;
+	public static final int MINUTES_IN_HOUR = 60;
+	public static final int HOURS_IN_DAY = 24;
+	public static final int DAYS_IN_YEAR = 365;
+	
+	public static final int DAYS_IN_JAN = 31;
+	public static final int DAYS_IN_FEB = 28;
+	public static final int DAYS_IN_MAR = 31;
+	public static final int DAYS_IN_APR = 30;
+	public static final int DAYS_IN_MAY = 31;
+	public static final int DAYS_IN_JUN = 30;
+	public static final int DAYS_IN_JUL = 31;
+	public static final int DAYS_IN_AUG = 31;
+	public static final int DAYS_IN_SEP = 30;
+	public static final int DAYS_IN_OCT = 31;
+	public static final int DAYS_IN_NOV = 30;
+	public static final int DAYS_IN_DEC = 31;
+	
+	public static final HashMap<Integer, Integer> DAYS_IN_EACH_MONTH = new HashMap<Integer, Integer>(){{
+		put(JANUARY, DAYS_IN_JAN);
+		put(FEBRUARY, DAYS_IN_FEB);
+		put(MARCH, DAYS_IN_MAR);
+		put(APRIL, DAYS_IN_APR);
+		put(MAY, DAYS_IN_MAY);
+		put(JUNE, DAYS_IN_JUN);
+		put(JULY, DAYS_IN_JUL);
+		put(AUGUST, DAYS_IN_AUG);
+		put(SEPTEMBER, DAYS_IN_SEP);
+		put(OCTOBER, DAYS_IN_OCT);
+		put(NOVEMBER, DAYS_IN_NOV);
+		put(DECEMBER, DAYS_IN_DEC);
+	}};
+	
+	public static final int JANUARY = 0;
+	public static final int FEBRUARY = 1;
+	public static final int MARCH = 2;
+	public static final int APRIL = 3;
+	public static final int MAY = 4;
+	public static final int JUNE = 5;
+	public static final int JULY = 6;
+	public static final int AUGUST = 7;
+	public static final int SEPTEMBER = 8;
+	public static final int OCTOBER = 9;
+	public static final int NOVEMBER = 10;
+	public static final int DECEMBER = 11;
+	
+	private Calendar calendar;
+	
+	private long longVal;
 
-	private ArrayList<Integer> value;
+	private int month;
+	private int date;
+	private int year;
+	private int hour;
+	private int minute;
+	private int second;
+	private int millisecond;
 	
 	public ModdedDate(int month, int date, int year){
-		this.value = new ArrayList<Integer>();
-		this.value.add(month);
-		this.value.add(date);
-		this.value.add(year);
+		
+		this.calendar = Calendar.getInstance();
+		this.setDate(year, month, date, 0, 0, 0, 0);
+		this.getValsFromCalendar();
 	}
 	
-	public ModdedDate(){
+	public ModdedDate() {
+		
 		Date now = new Date();
-		this.value = new ArrayList<Integer>();
-		this.value.add(now.getMonth()+1);
-		this.value.add(now.getDate());
-		this.value.add(now.getYear()+1900);
+		this.calendar = Calendar.getInstance();
+		this.calendar.setTime(now);
+		this.getValsFromCalendar();
 	}
 	
-	public ModdedDate(String dateString){
-			String[] data = dateString.split("/");
-			int[] intData = {Integer.valueOf(data[0]), Integer.valueOf(data[1]), Integer.valueOf(data[2])};
-			this.value = new ArrayList<Integer>();
-			this.value.add(intData[0]);
-			this.value.add(intData[1]);
-			this.value.add(intData[2]);
+	public ModdedDate(int month, int date, int year, int hour, int minute, int seconds, int millis) {
+		
+		this.calendar = Calendar.getInstance();
+		this.setDate(year, month, date, hour, minute, seconds, millis);
+		this.getValsFromCalendar();
+	}
+	
+	public void setDate(int year, int month, int date, int hour, int minute, int seconds, int millis) {
+		this.calendar.set(Calendar.YEAR, year);
+		
+		this.calendar.set(Calendar.MONTH, month);
+		
+		this.calendar.set(Calendar.DATE, date);
+		
+		this.calendar.set(Calendar.HOUR, hour);
+		
+		this.calendar.set(Calendar.MINUTE, minute);
+		
+		this.calendar.set(Calendar.SECOND, seconds);
+		
+		this.calendar.set(Calendar.MILLISECOND, millis);
+		
+		this.longVal = this.calendar.getTimeInMillis();
+	}
+	
+	public void getValsFromCalendar() {
+		
+		int y = this.calendar.get(Calendar.YEAR);
+		
+		int d = this.calendar.get(Calendar.DATE);
+		
+		int mon = this.calendar.get(Calendar.MONTH);
+		
+		int h = this.calendar.get(Calendar.HOUR);
+		
+		int min = this.calendar.get(Calendar.MINUTE);
+		
+		int s = this.calendar.get(Calendar.SECOND);
+		
+		int mil = this.calendar.get(Calendar.MILLISECOND);
+		
+		this.year = y;
+		this.date = d;
+		this.month = mon;
+		this.hour = h;
+		this.minute = min;
+		this.second = s;
+		this.millisecond = mil;
 	}
 	
 	public int compareTo(ModdedDate other){
-		if(other.year() == this.year()){
-			if(other.month() == this.month()){
-				if(other.date() == this.date()){
-					return 0;
-				}
-				else if(other.date() < this.date()){
-					return 1;
-				}
-				else{
-					return -1;
-				}
-			}
-			else if(other.month() < this.month()){
-				return 1;
-			}
-			else{
-				return -1;
-			}
-		}
-		else if(other.year() < this.year()){
-			return 1;
-		}
-		else{
-			return -1;
-		}
+
+		return this.calendar.compareTo(other.calendar);
+	}
+	
+	public boolean before(ModdedDate when){
+		
+		return this.calendar.before(when.calendar);
+	}
+	
+	public boolean after(ModdedDate when){
+		
+		return this.calendar.before(when.calendar);
 	}
 	
 	public int month(){
-		return this.value.get(0);
+		return this.month;
 	}
 	
 	public int date(){
-		return this.value.get(1);
+		return this.date;
 	}
 	
 	public int year(){
-		return this.value.get(2);
+		return this.year;
+	}
+	
+	public int hour(){
+		return this.hour;
+	}
+	
+	public int minute(){
+		return this.minute;
+	}
+	
+	public int seconds(){
+		return this.second;
+	}
+	
+	public int millis(){
+		return this.millisecond;
 	}
 	
 	public String toString(){
-		return (this.month() + "/" + this.date() + "/" + this.year());
+		return ( (this.month() + 1) + "/" + this.date() + "/" + this.year());
 	}
 }
