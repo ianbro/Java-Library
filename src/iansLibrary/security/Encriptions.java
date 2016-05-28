380f6fb9a932093cd08a2f3ca9f61d35a0d671ca
Ian Kirkpatrick <ianmann56@gmail.com>
2016-05-28 00:33:44 -0400

    fixed encriptions and decriptions

--- src/iansLibrary/security/Encriptions.java
+++ src/iansLibrary/security/Encriptions.java
@@ -1,125 +1,49 @@
 package iansLibrary.security;
 
-import java.util.ArrayList;
-
 public abstract class Encriptions {
 
-	public static String toShiftedNumberLine(String toEncript, int shiftVal){
-		if(shiftVal > 73){
-			return "cannot shift over 73 spaces";
-		}
-		String retVal = "";
-		for(int i = 0; i < toEncript.length(); i ++){
-			char c = toEncript.charAt(i);
-			int ascii;
-			String asciiString;
-			if(c == ' '){
-				ascii = 0 + shiftVal;
-			} else {
-				ascii = ((int) c) - 64 + shiftVal;
-			}
-			if(ascii < 10){
-				asciiString = "0" + ascii;
-			} else {
-				asciiString = String.valueOf(ascii);
-			}
-			retVal = retVal + asciiString;
-		}
-		return retVal;
-	}
-	
-	public static String toShiftedNumberLine(Password password, int shiftVal){
-		if(shiftVal > 73){
-			return "cannot shift over 73 spaces";
-		}
-		String toEncript = password.toString();
-		String retVal = "";
-		for(int i = 0; i < toEncript.length(); i ++){
-			char c = toEncript.charAt(i);
-			int ascii;
-			String asciiString;
-			if(c == ' '){
-				ascii = 0 + shiftVal;
-			} else {
-				ascii = ((int) c) - 64 + shiftVal;
-			}
-			if(ascii < 10){
-				asciiString = "0" + ascii;
-			} else {
-				asciiString = String.valueOf(ascii);
-			}
-			retVal = retVal + asciiString;
-		}
-		return retVal;
-	}
-	
-	public static String fromShiftedNumberLine(String toDecipher, int shiftVal){
-		ArrayList<String> string = new ArrayList<String>();
-		//shifting back down to normal
-		for(int i = 0; i < toDecipher.length(); i +=2){
-			string.add(toDecipher.substring(i, i+2));
-		}
-		ArrayList<String> string2 = new ArrayList<String>();
-		for(int i = 0; i < string.size(); i ++){
-			int newVal = Integer.valueOf(string.get(i)) - shiftVal;
-			string2.add(String.valueOf(newVal));
-		}
-		string = string2;
+	/**
+	 * * letters to ascii
+	 * * ascii nums += 7
+	 * * ascii nums *= 7
+	 * * put a random number at the beginning of the value of this letter
+	 * * put a random number at the end of the value of this letter
+	 * * flip the word around
+	 * @param str
+	 * @return
+	 */
+	public static String encript(String str){
+		char[] strArray = str.toCharArray();
+		String nstr = "";
+		String finalString = "";
 		
-		string2 = new ArrayList<String>();
-		for(int i = 0; i < string.size(); i ++){
-			if(string.get(i).equals("0")){
-				string2.add(" ");
+		for (int i = 0; i < strArray.length; i++) {
+			int ascii = (int) strArray[i];
+			ascii += 7;
+			ascii *= 7;
+			//adding a 3 to the end of ascii
+			String first = String.valueOf(Integer.valueOf((String.valueOf((int)(Math.random()*10)).substring(0, 1))) + 1).substring(0, 1);
+			String second = String.valueOf(Integer.valueOf((String.valueOf((int)(Math.random()*10)).substring(0, 1))) + 1).substring(0, 1);
+			ascii = Integer.valueOf(first + String.valueOf(ascii) + second);
+			
+			//add it to delimeteredArray with it's coresponding symbol
+			String finalVal = String.valueOf(ascii);
+			if (Character.isLetter(strArray[i])) {
+				finalVal = finalVal + "!";
 			}
-			else {
-				int ascii = (int) Integer.valueOf(string.get(i));
-				String c = Character.toString((char) (ascii + 64));
-				string2.add(c);
+			else if (Character.isDigit(strArray[i])) {
+				finalVal = finalVal + "*";
 			}
+			else{
+				finalVal = finalVal + "^";
+			}
+			nstr = nstr + finalVal;
 		}
 		
-		String retVal = "";
-		for(int i = 0; i < string2.size(); i ++){
-			retVal = retVal + string2.get(i);
+		//flipping around
+		for (int i = nstr.length() - 1; i >= 0; i --) {
+			finalString = finalString + String.valueOf(nstr.charAt(i));
 		}
-		
-		return retVal;
-	}
-	
-	public static Password fromShiftedNumberLine(Password toDecipher, int shiftVal){
-		String stringToDecipher = toDecipher.value;
-		ArrayList<String> string = new ArrayList<String>();
-		//shifting back down to normal
-		for(int i = 0; i < stringToDecipher.length(); i +=2){
-			string.add(stringToDecipher.substring(i, i+2));
-		}
-		for(int i = 0; i < string.size(); i ++){
-			System.out.print(string.get(i));
-		} System.out.println("");
-		ArrayList<String> string2 = new ArrayList<String>();
-		for(int i = 0; i < string.size(); i ++){
-			int newVal = Integer.valueOf(string.get(i)) - shiftVal;
-			string2.add(String.valueOf(newVal));
-		}
-		string = string2;
-		
-		string2 = new ArrayList<String>();
-		for(int i = 0; i < string.size(); i ++){
-			if(string.get(i).equals("0")){
-				string2.add(" ");
-			}
-			else {
-				int ascii = (int) Integer.valueOf(string.get(i));
-				String c = Character.toString((char) (ascii + 64));
-				string2.add(c);
-			}
-		}
-		
-		String retVal = "";
-		for(int i = 0; i < string2.size(); i ++){
-			retVal = retVal + string2.get(i);
-		}
-		
-		return new Password(retVal);
+		return finalString;
 	}
 }
